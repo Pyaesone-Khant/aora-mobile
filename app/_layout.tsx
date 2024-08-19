a1e3ea37ad3d1a2b2e3101fd4ca7be4f343bebf4
Pyaesone Khant <pyaesonekhant.dev@gmail.com>
2024-08-19 21:24:29 +0630

    config: setup nativewind & tailwindcss

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,32 @@
-import { Stack } from 'expo-router'
-import React from 'react'
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import React, { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 const RootPage = () => {
+
+  // loading fonts in rn
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if(error) throw error;
+
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error])
+
+  if(!fontsLoaded && !error) return null;  
+
   return (
     <Stack>
         <Stack.Screen name="index" options={{
